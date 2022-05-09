de3483c24adbb2aac929683ca7536e2304eb49d3
aquib-sh <shaikhaquib394@gmail.com>
2022-05-09 18:17:53 +0530

    Update tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,32 +4,32 @@ from translator import LanguageTranslator
 class TestE2F(unittest.TestCase):
     def test_null(self):
         translator = LanguageTranslator()
-        text = None
-        expected = None
+        text = "None"
+        expected = ""
         translation = translator.englishToFrench(text)
-        self.assertTrue(translation, expected)
+        self.assertNotEqual(translation, expected)
 
     def test_str(self):
         translator = LanguageTranslator()
         text = "Hello"
         expected = "Bonjour"
         translation = translator.englishToFrench(text)
-        self.assertTrue(translation, expected)
+        self.assertEqual(translation, expected)
 
 class TestF2E(unittest.TestCase):
     def test_null(self):
         translator = LanguageTranslator()
-        text = None
-        expected = None
+        text = "None"
+        expected = "" 
         translation = translator.frenchToEnglish(text)
-        self.assertTrue(translation, expected)
+        self.assertNotEqual(translation, expected)
 
     def test_str(self):
         translator = LanguageTranslator()
         text = "Bonjour"
         expected = "Hello"
         translation = translator.frenchToEnglish(text)
-        self.assertTrue(translation, expected)
+        self.assertEqual(translation, expected)
 
 if __name__ == "__main__":
     unittest.main()
